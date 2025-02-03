9e56b3fb183bd7a2d36e7f70a35063916c81d65a
satishccy <satishchoudhari13579@gmail.com>
2025-02-03 19:28:25 +0530

    Project initialised with AlgoKit CLI using template: https://github.com/algorandfoundation/algokit-fullstack-template.git

---
+++ projects/limit-order-contracts/__test__/LimitOrder.test.ts
@@ -0,0 +1,46 @@
+import { describe, test, expect, beforeAll, beforeEach } from '@jest/globals';
+import { algorandFixture } from '@algorandfoundation/algokit-utils/testing';
+import { Config } from '@algorandfoundation/algokit-utils';
+import { LimitOrderClient, LimitOrderFactory } from '../contracts/clients/LimitOrderClient';
+
+const fixture = algorandFixture();
+Config.configure({ populateAppCallResources: true });
+
+let appClient: LimitOrderClient;
+
+describe('LimitOrder', () => {
+  beforeEach(fixture.beforeEach);
+
+  beforeAll(async () => {
+    await fixture.beforeEach();
+    const { testAccount } = fixture.context;
+    const { algorand } = fixture;
+
+    const factory = new LimitOrderFactory({
+      algorand,
+      defaultSender: testAccount.addr,
+    });
+
+    const createResult = await factory.send.create.createApplication();
+    appClient = createResult.appClient;
+  });
+
+  test('sum', async () => {
+    const a = 13;
+    const b = 37;
+    const sum = await appClient.send.doMath({ args: { a, b, operation: 'sum' } });
+    expect(sum.return).toBe(BigInt(a + b));
+  });
+
+  test('difference', async () => {
+    const a = 13;
+    const b = 37;
+    const diff = await appClient.send.doMath({ args: { a, b, operation: 'difference' } });
+    expect(diff.return).toBe(BigInt(a >= b ? a - b : b - a));
+  });
+
+  test('hello', async () => {
+    const hello = await appClient.send.hello({ args: { name: 'world!' } });
+    expect(hello.return).toBe('Hello, world!');
+  });
+});
